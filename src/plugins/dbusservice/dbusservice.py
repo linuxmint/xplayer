83f2c1cf2d79b065b1769e161ec48c6c0c46ec4e
leigh123linux <leigh123linux@googlemail.com>
2018-05-12 10:45:47 +0100

    dbusservice: do not use unicode which does not exists in python 3

--- src/plugins/dbusservice/dbusservice.py
+++ src/plugins/dbusservice/dbusservice.py
@@ -57,9 +57,9 @@ class Root (dbus.service.Object): # pylint: disable-msg=R0923,R0904
         self.xplayer = xplayer
 
         self.null_metadata = {
-            'year' : u'', 'tracknumber' : '', 'location' : '',
-            'title' : u'', 'album' : u'', 'time' : u'', 'genre' : u'',
-            'artist' : u''
+            'year' : '', 'tracknumber' : '', 'location' : '',
+            'title' : '', 'album' : '', 'time' : '', 'genre' : '',
+            'artist' : ''
         }
         self.current_metadata = self.null_metadata.copy ()
         self.current_position = 0
@@ -120,11 +120,11 @@ class Root (dbus.service.Object): # pylint: disable-msg=R0923,R0904
                               title, album, num):
         self.current_metadata = self.null_metadata.copy ()
         if title:
-            self.current_metadata['title'] = unicode (title, 'utf-8')
+            self.current_metadata['title'] = title
         if artist:
-            self.current_metadata['artist'] = unicode (artist, 'utf-8')
+            self.current_metadata['artist'] = artist
         if album:
-            self.current_metadata['album'] = unicode (album, 'utf-8')
+            self.current_metadata['album'] = album
         if num:
             self.current_metadata['tracknumber'] = num
 
@@ -210,7 +210,7 @@ class Root (dbus.service.Object): # pylint: disable-msg=R0923,R0904
 
         raise dbus.exceptions.DBusException (
             'org.mpris.MediaPlayer2.UnknownInterface',
-            _(u'The MediaPlayer2 object does not implement the ‘%s’ interface')
+            _('The MediaPlayer2 object does not implement the ‘%s’ interface')
                 % interface_name)
 
     @dbus.service.method (dbus_interface = dbus.PROPERTIES_IFACE,
@@ -219,7 +219,7 @@ class Root (dbus.service.Object): # pylint: disable-msg=R0923,R0904
         if interface_name == 'org.mpris.MediaPlayer2':
             raise dbus.exceptions.DBusException (
                 'org.mpris.MediaPlayer2.ReadOnlyProperty',
-                _(u'The property ‘%s’ is not writeable.'))
+                _('The property ‘%s’ is not writeable.'))
         elif interface_name == 'org.mpris.MediaPlayer2.Player':
             if property_name == 'LoopStatus':
                 self.xplayer.action_remote_set_setting (
@@ -235,12 +235,12 @@ class Root (dbus.service.Object): # pylint: disable-msg=R0923,R0904
 
             raise dbus.exceptions.DBusException (
                 'org.mpris.MediaPlayer2.ReadOnlyProperty',
-                _(u'Unknown property ‘%s’ requested of a MediaPlayer 2 object')
+                _('Unknown property ‘%s’ requested of a MediaPlayer 2 object')
                     % interface_name)
 
         raise dbus.exceptions.DBusException (
             'org.mpris.MediaPlayer2.UnknownInterface',
-            _(u'The MediaPlayer2 object does not implement the ‘%s’ interface')
+            _('The MediaPlayer2 object does not implement the ‘%s’ interface')
                 % interface_name)
 
     @dbus.service.signal (dbus_interface = dbus.PROPERTIES_IFACE,
